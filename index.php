8d7ffbfb48cdda00e1a2c8b20ef66a5d7e2701cd
vitoria <vitoria.dev.f@gmail.com>
2023-02-17 23:53:34 -0300

    [add] arrumado

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include("banco/conexao.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de agenda de eventos</title>
-    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
